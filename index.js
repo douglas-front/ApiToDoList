e7cce02d5311c377c6249f5aea1ab6867a7a9ff3
douglas-front <douglasjknw007@gmail.com>
2023-12-27 09:04:13 -0300

    bug corrigido

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const index = express()
 
-app.use((req, res, next) => {
+index.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
