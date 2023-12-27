a27753f4830915571fd18c7448599dc12ddc3f7e
douglas-front <douglasjknw007@gmail.com>
2023-12-27 08:59:08 -0300

    cors atualizado

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const index = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
 index.use(express.json())
 
 require("./MongoConnect")
